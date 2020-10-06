b454edf3b825431d46238d098e5f7a569248341b
vangarita <vangarita@purolomo.com>
2020-10-06 16:44:39 -0400

    no logro dar con la falla capitulo 52

--- MiPrimeraAplicacionWeb/Scripts/GradoSeccion.js
+++ MiPrimeraAplicacionWeb/Scripts/GradoSeccion.js
@@ -165,6 +165,7 @@ function abrirModal(id) {
 
     }
     else {
+        alert("Se llamo desde el boton Editar")
         $.get("GradoSeccion/RecuperarInformacion/?id=" + id, function (data) {
 
             document.getElementById("txtIdGradoSeccion").value = data[0].IID;
@@ -173,7 +174,7 @@ function abrirModal(id) {
             document.getElementById("cboSeccion").value = data[0].IIDSECCION;
             alert("IIDSECCION " + data[0].IIDSECCION)
         });
-        alert("Se llamo desde el boton Editar")
+        //alert("Se llamo desde el boton Editar")
     }
 
 
