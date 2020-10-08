3ef5257125bf75ef7a2a5a7b45879abf5f18a8ea
vladimirangarita <vladimir.angarita@gmail.com>
2020-10-07 23:46:20 -0400

    capitulo 54

---
+++ MiPrimeraAplicacionWeb/Controllers/PeriodoGradoCursoController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionWeb.Controllers
+{
+    public class PeriodoGradoCursoController : Controller
+    {
+        // GET: PeriodoGradoCurso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ListarPeriodoGradoCurso()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+
+            var lista = from pgc in bd.PeriodoGradoCurso
+                        join per in bd.Periodo
+                        on pgc.IIDPERIODO equals per.IIDPERIODO
+                        join grand in bd.Grado
+                        on pgc.IIDGRADO equals grand.IIDGRADO
+                        join cur in bd.Curso
+                        on pgc.IIDCURSO equals cur.IIDCURSO
+                        select new
+                        {
+                            pgc.IID,
+                            NOMBREPERIODO = per.NOMBRE,
+                            NOMBREGRADO = grand.NOMBRE,
+                            NOMBRECURSO = cur.NOMBRE
+                        };
+            return Json(lista, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult RecuperarInformacion(int id)
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+
+            var lista = bd.PeriodoGradoCurso.Where(p => p.IID.Equals(id)).
+                Select(p => new
+                {
+                    p.IID,
+                    p.IIDPERIODO,
+                    p.IIDGRADO,
+                    p.IIDCURSO
+
+
+                });
+
+            return Json(lista, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult ListarPeriodo()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var lista = bd.Periodo.Where(p => p.BHABILITADO.Equals(1)).
+                Select(p => new
+                {
+                    p.IIDPERIODO,
+                    p.NOMBRE
+
+                });
+            return Json(lista, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public JsonResult ListarGrado()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var lista = bd.Grado.Where(p => p.BHABILITADO.Equals(1)).
+                Select(p => new
+                {
+                    p.IIDGRADO,
+                    p.NOMBRE
+
+                });
+            return Json(lista, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        public JsonResult ListarCurso()
+        {
+            PruebaDataContext bd = new PruebaDataContext();
+            var lista = bd.Curso.Where(p => p.BHABILITADO.Equals(1)).
+                Select(p => new
+                {
+                    p.IIDCURSO,
+                    p.NOMBRE
+
+                });
+            return Json(lista, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+    }
+}
---
+++ MiPrimeraAplicacionWeb/Scripts/PeriodoGradoCurso.js
@@ -0,0 +1,103 @@
+﻿
+
+Listar();
+
+
+function llenarCombo(data, control, primerElemento) {
+
+    var contenido = "";
+    if (primerElemento == true) {
+
+        contenido += "<option value=''>--Seleccione--</option>"
+
+    }
+    for (var i = 0; i < data.length; i++) {
+
+
+        contenido += "<option value='" + data[i].IID + "'>";
+
+
+        contenido += data[i].NOMBRE;
+
+        contenido += "</option>";
+
+
+    }
+
+
+    control.innerHTML = contenido;
+}
+
+function Listar() {
+
+
+
+    $.get("PeriodoGradoCurso/ListarPeriodoGradoCurso", function (data) {
+       
+
+        crearListado(["Id", "Periodo", "Grado", "Curso"], data);
+
+    })
+
+}
+
+
+
+function crearListado(arrayColumnas, data) {
+    var contenido = "";
+    contenido += "<table id='tablas'class='table'>"
+    contenido += "<thead>"
+
+    contenido += "<tr>"
+
+    for (var i = 0; i < arrayColumnas.length; i++) {
+        contenido += "<td>"
+        contenido += arrayColumnas[i];
+    }
+
+    contenido += "<td>Operaciones</td>"
+    contenido += "</tr>";
+
+    contenido += "</thead>"
+    var llaves = Object.keys(data[0]);
+    //alert(llaves);
+    contenido += "<tbody>"
+    for (var i = 0; i < data.length; i++) {
+
+        contenido += "<tr>"
+
+        for (var j = 0; j < llaves.length; j++) {
+            var valorLLaves = llaves[j];
+            contenido += "<td>";
+            contenido += data[i][valorLLaves];
+            contenido += "</td>";
+        }
+        var llaveId = llaves[0];
+        contenido += "<td>";
+        contenido += "<button class='btn btn-primary' onclick='abrirModal(" + data[i][llaveId] + ")' data-toggle='modal' data-target='#myModal'><i class='glyphicon glyphicon-edit'></i></button> "
+        contenido += "<button class='btn btn-danger' onclick='Eliminar(" + data[i][llaveId] + ")'><i class='glyphicon glyphicon-trash'></i></button>"
+        contenido += "</td>";
+
+        contenido += "</tr>";
+
+    }
+
+    contenido += "</tbody>"
+
+    contenido += "</table>"
+
+
+    document.getElementById("tabla").innerHTML = contenido;
+
+    $("#tablas").DataTable(
+
+        {
+
+            searching: false
+
+        }
+
+    );
+
+
+}
