4cc0fdea27f0e7269e26c3a454a52c5f60d3fe13
vladimirangarita <vladimir.angarita@gmail.com>
2020-11-16 01:50:05 -0400

    Capitulo 75 y 76 listo

--- MiPrimeraAplicacionWeb/Controllers/RolPaginaController.cs
+++ MiPrimeraAplicacionWeb/Controllers/RolPaginaController.cs
@@ -32,5 +32,43 @@ namespace MiPrimeraAplicacionWeb.Controllers
 
         }
 
+        public JsonResult ListarPaginas()
+        {
+            using (PruebaDataContext bd= new PruebaDataContext())
+            {
+                var lista = bd.Pagina.Where(p => p.BHABILITADO == 1)
+                    .Select(
+                    p => new
+                    {
+                        p.IIDPAGINA,
+                        p.MENSAJE,
+                        p.BHABILITADO
+                    }
+                    ).ToList();
+
+                return Json(lista, JsonRequestBehavior.AllowGet);
+            }
+
+            
+        }
+
+        public JsonResult ObtenerRol(int idRol)
+        {
+            using (PruebaDataContext bd=new PruebaDataContext())
+            {
+                var rol = bd.Rol.Where(p => p.IIDROL == idRol).Select(
+                    p => new
+                    {
+                        p.IIDROL,
+                        p.NOMBRE,
+                        p.DESCRIPCION
+                    }
+                    ).First();
+
+                return Json(rol, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
     }
 }
---
+++ MiPrimeraAplicacionWeb/Scripts/RolPagina.js
@@ -0,0 +1,102 @@
+﻿Listar();
+
+function Listar() {
+    $.get("RolPagina/ListarRol", function (data) {
+        crearListado(["Id Rol", "Nombre", "Descripcion"], data);
+    })
+}
+
+function BorrarDatos() {
+
+    var controles = document.getElementsByClassName("borrar");
+    //console.log(controles);
+    var ncontroles = controles.length;
+    for (var i = 0; i < ncontroles; i++) {
+        controles[i].value = "";
+    }
+}
+function abrirModal(id) {
+
+    var controlesObligatorios = document.getElementsByClassName("obligatorio");
+    var ncontroles = controlesObligatorios.length;
+
+    for (var i = 0; i < ncontroles; i++) {
+
+        controlesObligatorios[i].parentNode.classList.remove("error");
+
+
+    }
+
+
+    if (id == 0) {
+
+        BorrarDatos();
+
+    }
+    else {
+       
+    }
+
+
+    //alert(id);
+
+}
+
+function crearListado(arrayColumnas, data) {
+    var contenido = "";
+    contenido += "<table id='tablas'class='table'>"
+    contenido += "<thead>"
+
+    contenido += "<tr>"
+
+    for (var i = 0; i < arrayColumnas.length; i++) {
+        contenido += "<td>"
+        contenido += arrayColumnas[i];
+    }
+
+    contenido += "<td>Operaciones</td>"
+    contenido += "</tr>";
+
+    contenido += "</thead>"
+    var llaves = Object.keys(data[0]);
+    //alert(llaves);
+    contenido += "<tbody>"
+    for (var i = 0; i < data.length; i++) {
+
+        contenido += "<tr>"
+
+        for (var j = 0; j < llaves.length; j++) {
+            var valorLLaves = llaves[j];
+            contenido += "<td>";
+            contenido += data[i][valorLLaves];
+            contenido += "</td>";
+        }
+        var llaveId = llaves[0];
+        contenido += "<td>";
+        contenido += "<button class='btn btn-primary' onclick='abrirModal(" + data[i][llaveId] + ")' data-toggle='modal' data-target='#myModal'><i class='glyphicon glyphicon-edit'></i></button> "
+        contenido += "<button class='btn btn-danger' onclick='Eliminar(" + data[i][llaveId] + ")'><i class='glyphicon glyphicon-trash'></i></button>"
+        contenido += "</td>";
+
+        contenido += "</tr>";
+
+    }
+
+    contenido += "</tbody>"
+
+    contenido += "</table>"
+
+
+    document.getElementById("tabla").innerHTML = contenido;
+
+    $("#tablas").DataTable(
+
+        {
+
+            searching: false
+
+        }
+
+    );
+
+
+}
